6197a30cab32e5776f30eb3f5710dc71b3c53da5
saikumar padamati <saikumarpadamati@saikumars-MacBook-Pro.local>
2024-08-08 18:56:17 -0400

    added & tested CRUD operations

--- src/main/java/com/secure/notes/config/SecurityConfig.java
+++ src/main/java/com/secure/notes/config/SecurityConfig.java
@@ -3,24 +3,24 @@ package com.secure.notes.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) ->
-                requests.requestMatchers("/contact").permitAll() //avoids security check on /contact endpint
-                        .requestMatchers("/public/**").permitAll()
-                        .requestMatchers("/admin").denyAll()
-                        .anyRequest().authenticated());
-        http.sessionManagement(session->{
+                requests.anyRequest().authenticated());
+        http.csrf(csrf-> csrf.disable());
+       /* http.sessionManagement(session->{
             session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         });
-       // http.formLogin(withDefaults()); //disable this if you dont want form based login
+        http.formLogin(withDefaults()); //disable this if you dont want form based login */
         http.httpBasic(withDefaults());
         return http.build();
     }
